87f064edf585418adb89dfd12c746c6390b70949
123isi <123isi@naver.com>
2025-09-08 20:52:20 +0900

    fix :: teamspace

--- src/main/java/org/example/yoyager_backend/controller/AuthController.java
+++ src/main/java/org/example/yoyager_backend/controller/AuthController.java
@@ -6,8 +6,10 @@ import org.example.yoyager_backend.controller.Request.RegisterRequest;
 import org.example.yoyager_backend.controller.Response.LoginResponse;
 import org.example.yoyager_backend.controller.Response.RegisterResponse;
 import org.example.yoyager_backend.service.LoginService;
+import org.example.yoyager_backend.service.LogoutService;
 import org.example.yoyager_backend.service.RefreshService;
 import org.example.yoyager_backend.service.SignUpService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -18,6 +20,7 @@ public class AuthController {
   private final SignUpService signUpService;
   private final LoginService loginService;
   private final RefreshService refreshService;
+  private final LogoutService logoutService;
   @PostMapping("/signup")
   public RegisterResponse signUp(@RequestBody RegisterRequest request) {
     return signUpService.signUp(request);
@@ -26,4 +29,9 @@ public class AuthController {
   public LoginResponse login(@RequestBody LoginRequest request) {return loginService.login(request);}
   @GetMapping("/refresh")
   public String refresh(@RequestHeader("refresh_token") String refreshToken) {return refreshService.refresh(refreshToken);}
+  @DeleteMapping("/logout")
+  public ResponseEntity<String> logout(@RequestHeader("access_token") String accessToken) {
+    logoutService.logout(accessToken);
+    return ResponseEntity.ok("삭제되었습니다");
+  }
 }
